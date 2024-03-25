1529485c9633f6e7c0c4bbf521e3ccefc27fc1d7
pilfered-words <thebluejay@gmail.com>
2024-03-25 17:10:50 -0400

    expanded version

--- python_dev_exercise/app.py
+++ python_dev_exercise/app.py
@@ -12,18 +12,37 @@ def home():
 def search():
     # this can be a request to display the first name form (with GET) or submitting the form (with POST)
     if request.method == 'POST':
-        firstName = request.form['firstName']
-        fullData = pandas.read_csv(DATA_PATH)
-
-        # for a substring of a patient's name to work to find them, use:
-        # data = fullData[fullData['PatientFirstName'].str.contains(firstName, case=False, regex=False)]
-
-        data = fullData[fullData['PatientFirstName'].str.lower() == firstName.lower()]
-        data = data.drop_duplicates(subset=['PatientID', 'MostRecentTestDate', 'TestName'])
-
-        # set class for bootstrap formatting, handle case when no results
-        data_html = data.to_html(classes="table col-sm-10") if data.size > 0 else "No results found for that name."
-        
-        return render_template("search.html", data_html = data_html, firstName = firstName)
+        searchQuery = request.form['name']
     else:
-        return render_template("search.html")
+        searchQuery = ''
+    fullData = pandas.read_csv(DATA_PATH)
+
+    patients = fullData.drop_duplicates(subset=['PatientID'])[['PatientID', 'PatientLastName', 'PatientFirstName', 'Gender']]
+    patients = add_full_name(patients)
+
+    # filter patients to match query
+    patients = patients[patients['fullname'].str.contains(searchQuery, case=False, regex=False)]
+
+    # set class for bootstrap formatting, handle case when no results
+    data_html=patients.to_html(classes="table col-sm-10")
+    
+    return render_template("search.html", patients = patients, searchQuery = searchQuery)
+    
+@app.route('/tests/<int:patientId>', methods=['GET'])    
+def tests_by_patient(patientId):
+    fullData = pandas.read_csv(DATA_PATH)
+
+    tests = fullData[fullData['PatientID']==patientId]
+    if len(tests) == 0:
+        return render_template("tests.html", data_html="<p>No test results found for this id</p>")
+    tests = tests.drop_duplicates(subset=['PatientID', 'MostRecentTestDate', 'TestName'])
+    
+    fullname = add_full_name(tests).iloc[0]['fullname']
+    data_html = tests[['PatientID','PatientLastName','PatientFirstName','Gender','MostRecentTestDate','TestName',
+                       'MostRecentLabResult']].to_html(classes="table col-sm-10", index=False, border=0)
+    return render_template("tests.html", data_html=data_html, fullname=fullname)
+
+
+def add_full_name(frame):
+    result = frame.assign(fullname=frame["PatientLastName"] + ', ' + frame["PatientFirstName"])
+    return result[['PatientID', 'fullname', 'Gender']]
